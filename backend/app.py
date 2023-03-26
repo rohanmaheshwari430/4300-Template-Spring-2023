ea85a3ec488fce9e6d6e1427bf35af98a20b7ac3
Chris Gu <chrisgu12321@gmail.com>
2023-03-26 18:05:47 -0400

    undo

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 import json
 import os
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request
 from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 
@@ -12,9 +12,9 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
 # Don't worry about the deployment credentials, those are fixed
 # You can use a different DB name if you want to
 MYSQL_USER = "root"
-MYSQL_USER_PASSWORD = ""
+MYSQL_USER_PASSWORD = "MayankRao16Cornell.edu"
 MYSQL_PORT = 3306
-MYSQL_DATABASE = "songmilarity"
+MYSQL_DATABASE = "kardashiandb"
 
 mysql_engine = MySQLDatabaseHandler(
     MYSQL_USER, MYSQL_USER_PASSWORD, MYSQL_PORT, MYSQL_DATABASE)
