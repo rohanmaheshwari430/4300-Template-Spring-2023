b58eec317dd1e993544cd23b3dc68c25379c3c90
Rohan Maheshwari <rohanm@gmail.com>
2023-05-01 22:02:19 -0400

    remove songs w no lyrics

--- backend/makelowercase.py
+++ backend/makelowercase.py
@@ -1,15 +1,42 @@
 import json
 
-# Open the data.json file and load its contents into a dictionary
-with open('data.json', 'r') as f:
-    data = json.load(f)
+# def make_lowercase():
+#   # Open the data.json file and load its contents into a dictionary
+#   with open('data.json', 'r') as f:
+#       data = json.load(f)
 
-# Convert all keys in the "lyrics" attribute to lowercase
-if 'lyrics' in data:
-    lyrics_dict = data['lyrics']
-    lyrics_dict = {k.lower(): v for k, v in lyrics_dict.items()}
-    data['lyrics'] = lyrics_dict
+#   # Convert all keys in the "lyrics" attribute to lowercase
+#   if 'lyrics' in data:
+#       lyrics_dict = data['lyrics']
+#       lyrics_dict = {k.lower(): v for k, v in lyrics_dict.items()}
+#       data['lyrics'] = lyrics_dict
 
-# Save the updated data back to the data.json file
-with open('data.json', 'w') as f:
-    json.dump(data, f)
+#   # Save the updated data back to the data.json file
+#   with open('data.json', 'w') as f:
+#       json.dump(data, f)
+
+def remove_no_lyric_songs():
+    lyrics = {
+        "please": 1,
+        "back": 1,
+        "lyrics": 1,
+        "released": 2,
+        "yet": 1,
+        "check": 1,
+        "song": 2
+      }
+    with open('data.json', 'r') as f:
+        data = json.load(f)
+    songs = data['songs']
+    for song in songs:
+        if song['lyrics'] == lyrics:
+            print(song['title'])
+            songs.remove(song)
+    data['songs'] = songs
+    with open('data.json', 'w') as f:
+        json.dump(data, f)
+
+remove_no_lyric_songs()
+
+
+
